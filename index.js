bf24dc1c153b3f5a44c568d73f245ca372a52ad2
Carlos Beato Rioja <carbeario@alumn.us.es>
2024-02-24 18:15:54 +0100

    /samples/CBR

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ let cool=require("cool-ascii-faces");
 
 let app = express();
 let faces= cool();
-const port=10000
+
+const port=(process.env.PORT || 10000);
 
 
 
 app.use("/",express.static("./public"));
 
-
 app.get("/cool",(req,res)=>{
     res.send(faces);
-}
+});
 
-)
-
-
-app.listen(port);
+app.listen(port,()=>{
+    console.log(`Server listening on port ${port}`)
+});
