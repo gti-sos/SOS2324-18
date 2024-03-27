a43c294eddfbaf761864dfab90be2a410de27e53
Carlos Beato Rioja <carbeario@alumn.us.es>
2024-03-27 19:37:35 +0100

    Cambios F07 carbeario

---
+++ front/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Header from "./header.svelte";
+    import { Styles } from '@sveltestrap/sveltestrap';
+    
+
+</script>
+
+
+<Styles/>  
+<Header/>
+<h1>Welcome</h1>
+
+<slot></slot>
+
+
