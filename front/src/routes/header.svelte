57f76822bfdbcb2716f8d7627dfa49289d176850
Carlos Beato Rioja <carbeario@alumn.us.es>
2024-03-27 19:37:35 +0100

    Cambios F07 carbeario

---
+++ front/src/routes/header.svelte
@@ -0,0 +1,10 @@
+
+<script>
+    import { Nav, NavItem, NavLink } from '@sveltestrap/sveltestrap';
+</script>
+
+
+<Nav>
+    <NavItem active><NavLink href="/">Home</NavLink></NavItem>
+    <NavItem active><NavLink href="/regional-politicies-acceptance">Votes</NavLink></NavItem>
+</Nav>
