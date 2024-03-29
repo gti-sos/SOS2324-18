79c6975dc6b10dc494180bee239a6e05f6305146
Carlos Beato Rioja <carbeario@alumn.us.es>
2024-03-29 14:04:17 +0100

    Cambios en el frontend carbeario

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,11 +1,11 @@
 
 <h1>SOS2324-18</h1>
     <h2>Team</h2>
-        <h3><a href="https://github.com/carbeario">Carlos Beato Rioja</a></h3>
+        <h3><a href="https://github.com/carbeario">Carlos Beato Rioja</a> <a href="api/v1/regional-politicies-acceptance">- api/v1/regional-politicies-acceptance</a></h3>
         <h3><a href="https://github.com/migmormar7">Miguel Moreno Martín-Palomo</a></h3>
         <h3><a href="https://github.com/JJoaquinGV19">José Joaquín Garrido Vargas</a> - foods-prices-inflation</h3>
     <h2>FrontEnd</h2>
-        <h3><a href="https://github.com/carbeario">Carlos Beato Rioja</a></h3>
+        <h3><a href="./regional-politicies-acceptance">regional-politicies-acceptance</a></h3>
         <h3><a href="https://github.com/migmormar7">Miguel Moreno Martín-Palomo</a></h3>
         <h3><a href="./foods-prices-inflation">Foods Prices Inflation</a></h3>
     <h2>Project description</h2>
