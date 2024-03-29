c50720bd5e229ea2a338d896e35af604553aff48
JJoaquinGV19 <jjoaquingv19@gmail.com>
2024-03-29 20:12:04 +0100

    Nuevas funcionalidades

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
     import Header from "./header.svelte";
     import { Styles } from '@sveltestrap/sveltestrap';
-    
+    import { Button, Icon, colorMode } from '@sveltestrap/sveltestrap';
 
 </script>
 
@@ -9,7 +9,13 @@
 <Styles/>  
 <Header/>
 <h1>Bienvenido</h1>
-
+<Button color="primary" outline active={$colorMode === 'light'} on:click={() => ($colorMode = 'light')}>
+light <Icon name="sun-fill" />
+</Button>
+<Button color="primary" outline active={$colorMode === 'dark'} on:click={() => ($colorMode = 'dark')}>
+dark <Icon name="moon-stars-fill" />
+</Button>
+<p></p>
 <slot></slot>
 
 
