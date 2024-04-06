62bb70fc1c8810d80de8081aadd3f78cd90ba24f
JJoaquinGV19 <jjoaquingv19@gmail.com>
2024-04-06 13:40:35 +0200

    e2e

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,20 +1,29 @@
 <script>
     import Header from "./header.svelte";
-    import { Styles } from '@sveltestrap/sveltestrap';
-    import { Button, Icon, colorMode } from '@sveltestrap/sveltestrap';
+    import { CardText, Styles } from '@sveltestrap/sveltestrap';
+    import { Button, Icon, colorMode, Card, Container } from '@sveltestrap/sveltestrap';
 
 </script>
     
-    
+<svelte:head>
+    <title>SOS2324-28 Manager</title>
+</svelte:head>
+
 <Styles/>  
 <Header/>
-<h1>Bienvenido!</h1>
-<Button color="primary" outline active={$colorMode === 'light'} on:click={() => ($colorMode = 'light')}>
-Claro <Icon name="sun-fill" />
-</Button>
-<Button color="primary" outline active={$colorMode === 'dark'} on:click={() => ($colorMode = 'dark')}>
-Oscuro <Icon name="moon-stars-fill" />
-</Button>
+<Card theme="auto">
+    <CardText>
+    <h2>Bienvenido! 
+        <Button color="primary" outline active={$colorMode === 'light'} on:click={() => ($colorMode = 'light')}>
+        Claro <Icon name="sun-fill" />
+        </Button>
+        <Button color="primary" outline active={$colorMode === 'dark'} on:click={() => ($colorMode = 'dark')}>
+        Oscuro <Icon name="moon-stars-fill" />
+        </Button>
+    </h2>
+    </CardText>
+</Card>
+
 <p></p>
 <slot></slot>
 
