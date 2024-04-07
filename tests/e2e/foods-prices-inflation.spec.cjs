1a4508b97f9d56334fc7698f872acc3ae33c9881
JJoaquinGV19 <jjoaquingv19@gmail.com>
2024-04-07 13:34:46 +0200

    e2e

--- tests/e2e/foods-prices-inflation.spec.cjs
+++ tests/e2e/foods-prices-inflation.spec.cjs
@@ -8,10 +8,12 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/SOS2324-28/);
 });
 
-test('List Foods', async ({ page }) => {
+test('Mas de 10 registros', async ({ page }) => {
   await page.goto('http://localhost:10000/foods-prices-inflation');
 
-  //await page.waitForTimeout(2000);
+  let boton = await page.locator('.load');
+  await boton.click();
+  await page.waitForTimeout(2000);
   let foodCount = (await page.locator('.foodItem').all()).length;
 
   // Expects page to have a heading with the name of Installation.
