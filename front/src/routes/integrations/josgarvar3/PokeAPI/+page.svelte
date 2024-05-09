a0166eda2a3e4dfe75f4007b4202738381f6933f
JJoaquinGV19 <jjoaquingv19@gmail.com>
2024-05-09 11:17:25 +0200

    Integracion

---
+++ front/src/routes/integrations/josgarvar3/PokeAPI/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let pokes = [];
+
+	onMount(async () => {
+		const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=50&offset=50', {
+			method: "GET"
+		});
+		const data = await response.json();
+		// Procesa los datos según el formato que necesite Highcharts
+		pokes = data.results;
+        console.log(pokes);
+	});
+</script>
